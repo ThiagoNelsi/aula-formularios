0d422c072b9ba1fa731bc557747f1cd9569595b7
ThiagoNelsi <thiagonelsi@gmail.com>
2023-01-19 22:12:49 -0300

    feat: codigo desenvolvido na aula

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 app.post('/cadastro', (req, res) => {
     console.log(req.body);
     res.send('Cadastrado!');
 });
 
-app.listen(3333);
+app.listen(3333, () => console.log('Rodando'));
